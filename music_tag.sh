8dd6fd855f4a383c823aa76b8053586abe8958ef
Antoine Cordier <acordier@pop-os.localdomain>
2018-12-30 17:45:01 +0100

    fix: handle properly list of files in music_tag

--- music_tag.sh
+++ music_tag.sh
@@ -3,7 +3,38 @@
 ## using metaflac if file has a flac extension
 ## using mid3v2 if file has a mp3 extension
 
-parse_arguments() {
+
+tag_flac() {
+  cmd="metaflac"  
+  if [ -n "$ARTIST" ]; then cmd="${cmd} --remove-tag ARTIST --set-tag ARTIST=${ARTIST}"; fi
+  if [ -n "$GENRE" ]; then cmd="${cmd} --remove-tag GENRE --set-tag GENRE=${GENRE}"; fi
+  if [ -n "$ALBUM_ARTIST" ]; then cmd="${cmd} --remove-tag ALBUM_ARTIST --set-tag ALBUM_ARTIST=${ALBUM_ARTIST}"; fi
+  echo "Running $cmd $1"
+  eval "$cmd $(printf '%q' $1)"
+}
+
+tag_mp3() {
+  cmd="mid3v2"
+  if [ -n "$ARTIST" ]; then cmd="${cmd} -a ${ARTIST}"; fi
+  if [ -n "$GENRE" ]; then cmd="${cmd} -g ${GENRE}"; fi
+  if [ -n "$ALBUM_ARTIST" ]; then cmd="${cmd} --TPE2 ${ALBUM_ARTIST}"; fi
+  echo "Running $cmd $1"
+  eval "$cmd $(printf '%q' $1)"
+}
+
+tag_file() {
+  if [[ "$1" == *.flac ]]; then
+    echo "Editing tags (assuming FLAC file from extension)"
+    tag_flac "$1"
+  fi
+
+  if [[ "$1" == *.mp3 ]]; then
+    echo "Editing tags (assuming mp3 file from extension)"
+    tag_mp3 "$1"
+  fi
+}
+
+main() {
   while getopts a:c:g: option
   do
     case "${option}" in
@@ -14,42 +45,13 @@ parse_arguments() {
   done
 
   shift "$((OPTIND - 1))"
-  FILE=$1
-  if [ -z "$FILE" ]; then 
-    echo "File name is missing";
-    exit 1;
-  fi
-}
-
-tag_flac() {
-  cmd="metaflac"  
-  if [ -n "$ARTIST" ]; then cmd="${cmd} --remove-tag ARTIST --set-tag ARTIST=${ARTIST}"; fi
-  if [ -n "$GENRE" ]; then cmd="${cmd} --remove-tag GENRE --set-tag GENRE=${GENRE}"; fi
-  if [ -n "$ALBUM_ARTIST" ]; then cmd="${cmd} --remove-tag ALBUM_ARTIST --set-tag ALBUM_ARTIST=${ALBUM_ARTIST}"; fi
-  echo "Running $cmd $FILE"
-  $($cmd "$FILE")
-}
-
-tag_mp3() {
-  cmd="mid3v2"
-  if [ -n "$ARTIST" ]; then cmd="${cmd} -a ${ARTIST}"; fi
-  if [ -n "$GENRE" ]; then cmd="${cmd} -g ${GENRE}"; fi
-  if [ -n "$ALBUM_ARTIST" ]; then cmd="${cmd} --TPE2 ${ALBUM_ARTIST}"; fi
-  echo "Running $cmd $FILE"
-  $($cmd "$FILE")
-}
-
-main() {
-  parse_arguments "$@"
-  if [[ "$FILE" == *.flac ]]; then
-    echo "Editing tags: assuming FLAC file"
-    tag_flac
-  fi
-
-  if [[ "$FILE" == *.mp3 ]]; then
-    echo "Editng tags: assuming mmp3 file"
-    tag_mp3
-  fi
+  
+  ((!$#)) && echo "File name is required"
+  
+  OLD_IFS=$IFS
+  IFS=$'\n'
+  for file in $@; do tag_file "$file"; done
+  IFS=$OLD_IFS
 }
 
 main "$@"
